ede10e1e3a21ef48083f3e874a4e3ef347a38e1c
Joel Håkansson <joel.hakansson@mtm.se>
2016-12-13 14:06:11 +0100

    Adds equals/hashcode to TaskOption and TaskOptionValue (fixes #3)

--- src/org/daisy/dotify/api/tasks/TaskOption.java
+++ src/org/daisy/dotify/api/tasks/TaskOption.java
@@ -138,4 +138,34 @@ public final class TaskOption {
 		return valuesSet;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		TaskOption other = (TaskOption) obj;
+		if (key == null) {
+			if (other.key != null) {
+				return false;
+			}
+		} else if (!key.equals(other.key)) {
+			return false;
+		}
+		return true;
+	}
+
 }
