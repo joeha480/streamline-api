d31eeab119a811c20ce06da0a5d51cc66711a3a9
Joel Håkansson <joel.hakansson@mtm.se>
2018-01-02 16:38:18 +0100

    WIP: fileset

---
+++ src/org/daisy/streamline/api/media/FileLocation.java
@@ -0,0 +1,33 @@
+package org.daisy.streamline.api.media;
+
+import java.io.File;
+
+class FileLocation {
+	private final String value;
+	FileLocation(File value) {
+		this.value = value.getAbsolutePath();
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FileLocation other = (FileLocation) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+}
